329209b55253d5e7fdb72880062590351bc2a579
BRPersonal <balajirengan@gmail.com>
2024-11-26 18:36:04 +0530

    coding WIP

--- analytics.py
+++ analytics.py
@@ -17,18 +17,28 @@ def fetch_analytics_data() -> pandas.core.frame.DataFrame :
     # Fetch all data from the Redis list
     raw_data = redis_client.lrange(analytics_key, 0, -1)
 
+    only_first_time = True
     for encoded_data in raw_data:
         unpacked_data = msgpack.unpackb(encoded_data,raw=False)
         print("-" * 50)
 
-        if (type(unpacked_data) == dict):
-            print("keys:", unpacked_data.keys())
+        api_key = unpacked_data.get('APIKey')
+        if not api_key:
+            continue #skip invalid api keys
 
-        apiKey = unpacked_data.get('APIKey')
-        if apiKey:
-            print(f"apiKey={apiKey}")
-            print(f"apiName={unpacked_data.get('APIName')}")
-            print(f"apiId={unpacked_data.get('APIID')}")
+        print(f"apiKey={api_key}")
+        print(f"apiName={unpacked_data.get('APIName')}")
+        print(f"apiId={unpacked_data.get('APIID')}")
+        print(f"host={unpacked_data.get('Host')}")
+        print(f"path={unpacked_data.get('Path')}")
+        print(f"responseCode={unpacked_data.get('ResponseCode')}")
+
+        print(f"Day={unpacked_data.get('Day')}")
+        print(f"Month={unpacked_data.get('Month')}")
+        print(f"TimeStamp={unpacked_data.get('TimeStamp')}")
+        print(f"OrgID={unpacked_data.get('OrgID')}")
+
+
 
 
     df = pd.DataFrame()
